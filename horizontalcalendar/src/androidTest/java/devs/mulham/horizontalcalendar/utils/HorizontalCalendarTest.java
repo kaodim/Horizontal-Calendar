b6f1bb9aa9919d35e736333bc9196e68dc10043a
Shasin <shasin89@gmail.com>
2018-01-18 12:27:04 +0800

    Added unit test

--- horizontalcalendar/src/androidTest/java/devs/mulham/horizontalcalendar/utils/HorizontalCalendarTest.java
+++ horizontalcalendar/src/androidTest/java/devs/mulham/horizontalcalendar/utils/HorizontalCalendarTest.java
@@ -3,6 +3,7 @@ package devs.mulham.horizontalcalendar.utils;
 import android.test.AndroidTestRunner;
 import static org.mockito.Mockito.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -51,11 +52,24 @@ public class HorizontalCalendarTest {
     }
 
     @Test
-    public void testIfGoPreviousDayInvokeScrollToPosition(){
+    public void testIfGoPreviousDayInvokeScrollToPositi(){
         int position = 40;
         Calendar date = Calendar.getInstance();
         horizontalCalendar.goPreviousDay(date);
         verify(horizontalCalendar,times(1)).scrollToPositionWithNoAnimation(position);
     }
+
+    @Test
+    public void testIfGetWeekDayReturnsCorrectValue(){
+        Assert.assertEquals(6,horizontalCalendar.getWeekDayNumber("Sunday"));
+        Assert.assertEquals(5,horizontalCalendar.getWeekDayNumber("Monday"));
+        Assert.assertEquals(4,horizontalCalendar.getWeekDayNumber("Tuesday"));
+        Assert.assertEquals(3,horizontalCalendar.getWeekDayNumber("Wednesday"));
+        Assert.assertEquals(2,horizontalCalendar.getWeekDayNumber("Thursday"));
+        Assert.assertEquals(1,horizontalCalendar.getWeekDayNumber("Friday"));
+        Assert.assertEquals(0,horizontalCalendar.getWeekDayNumber("Saturday"));
+    }
+
+
 
 }
